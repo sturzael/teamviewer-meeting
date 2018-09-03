de7cec3b59bd1706200359fe097a74fca929075a
sturzael <esturz23@gmail.com>
2018-09-04 10:47:16 +1200

    got a 405 - added auth token headers

--- master.js
+++ master.js
@@ -1,11 +1,11 @@
-alert('connected');
-
 var APIKey;
 
 jQuery(document).ready(function($) {
 
+
+
   $.ajax({
-    url: "/data.json",
+    url: "data.json",
     dataType: "json",
     beforeSend: function(xhr) {
       if (xhr.overrideMimeType) {
@@ -15,18 +15,34 @@ jQuery(document).ready(function($) {
     success: function(DataFromJson) {
       APIKey = DataFromJson.apikey;
       console.log(APIKey);
-      // start();
+      addHeaders();
     },
     error: function() {
-      console.log("Something Went Wrong");
+      console.log("Something Went Wsrong");
     }
   });
 
+  function addHeaders() {
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', APIKey);
+
+    var myInit = {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    var myRequest = new Request(APIKey, myInit);
+
+    myContentType = myRequest.headers.get('Authorization');
+    start();
+  };
+
   function start() {
     $.ajax({
-      url: 'URL',
-      type: 'POST',
-      data: 'DATA',
+      url: 'https://webapi.teamviewer.com/api/v1/ping',
+      type: 'GET',
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       success: function(DataFromJson) {
