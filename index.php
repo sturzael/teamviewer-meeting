fc1f59d36576b548f71554e777d9fdc9d7e43191
sturzael <esturz23@gmail.com>
2018-09-05 11:50:36 +1200

    creating an blizz meeting functioning

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ $data = file_get_contents("data.json");
 $decodedData = json_decode($data, true);
 $apikey = $decodedData['apikey'];
 
-$uri = "https://webapi.teamviewer.com/api/v1/meetings";
-$response = \Httpful\Request::get($uri)
+$uri = "https://webapi.teamviewer.com/api/v1/meetings/blizz";
+$response = \Httpful\Request::post($uri)
     ->addHeader('Authorization', "Bearer $apikey")
+    ->sendsType(\Httpful\Mime::FORM)
+    ->withoutStrictSsl()
+    ->expectsJson()
+    ->body('subject=test&start=2018-09-08T14:00:00Z&end=2018-09-08T15:00:00Z')
     ->send();
 echo $response;
 
