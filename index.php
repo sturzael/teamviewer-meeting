41b0200039d19e1757834db118463816b7a81cd0
sturzael <esturz23@gmail.com>
2018-09-04 16:23:00 +1200

    removed html

--- index.php
+++ index.php
@@ -12,16 +12,3 @@ $response = \Httpful\Request::get($uri)
 echo $response;
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <?= 'Only used for local tests - You need to change your locations inside script / php files as well.'?>
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-        <!-- <script type="text/javascript" src="master.js"></script> -->
-  </body>
-</html>
