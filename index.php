44008e996bd63388266bca9106460e353ddd8cc7
sturzael <esturz23@gmail.com>
2018-09-04 17:07:20 +1200

    end of day

--- index.php
+++ index.php
@@ -5,10 +5,17 @@ $data = file_get_contents("data.json");
 $decodedData = json_decode($data, true);
 $apikey = $decodedData['apikey'];
 
-$uri = "https://webapi.teamviewer.com/api/v1/account";
+$uri = "https://webapi.teamviewer.com/api/v1/meetings";
 $response = \Httpful\Request::get($uri)
     ->addHeader('Authorization', "Bearer $apikey")
     ->send();
 echo $response;
+
+// $uri = "https://webapi.teamviewer.com/api/v1/meetings";
+// $response = \Httpful\Request::get($uri)
+//     ->addHeader('Authorization', "Bearer $apikey")
+//     ->send();
+// echo $response;
+
 
 ?>
