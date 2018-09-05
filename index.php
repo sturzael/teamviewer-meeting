ab2edec1ab5932bfd65d4b459486ad43826ca564
sturzael <esturz23@gmail.com>
2018-09-05 16:09:28 +1200

    ready to test againest db

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
+
+define('SHORTINIT', true);
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-config.php');
+
+global $wpdb
+
 include('httpful.phar');
 
 $data = file_get_contents("data.json");
 $decodedData = json_decode($data, true);
 $apikey = $decodedData['apikey'];
 
-
-
 //fetch date times from wordpress and set times below
-
-
+$bookings = $wpdb->get_results("SELECT * FROM `wp_amelia_appointments`", ARRAY_A);
+print_r($bookings);
 
 $startTime = "2018-09-08T14:00:00Z";
 $endTime = "2018-09-08T15:00:00Z";
 $subject = "subjectTest";
 
 //fetch all meetings
+
+function checkMeeting(){
 $uri = "https://webapi.teamviewer.com/api/v1/meetings/blizz";
 $response = \Httpful\Request::get($uri)
     ->addHeader('Authorization', "Bearer $apikey")
@@ -28,6 +35,8 @@ foreach ($decodedResponse['blizzMeetings'] as $item) {
 }
 
 (in_array($startTime, $meetingTime) && in_array($endTime, $meetingEndTime)) ? 'in array' : createMeeting();
+}
+
 
 function createMeeting(){
   $uri = "https://webapi.teamviewer.com/api/v1/meetings/blizz";
